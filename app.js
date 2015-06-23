9e8afa2aa5c05af33dc785cb947c871b17ed1a90
Justin Beuck <jbeuck@yahoo.com>
2015-06-23 15:50:44 -0500

    removed files

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ app.get('/contact',function(req,res){
   res.sendFile(path.join(__dirname+'/views/contact.html'));
 });
 
-// app.get('/schedule',function(req,res){
-//   res.sendFile(path.join(__dirname+'/views/schedule.html'));
-// });
-
-// app.get('/signup',function(req,res){
-//   res.sendFile(path.join(__dirname+'/views/signup.html'));
-// });
-
 app.use("/css", express.static(__dirname + '/css'));
 
 app.use("/js", express.static(__dirname + '/js'));
