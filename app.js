740c1caec80fc1fe9ebe74b4a7d7e7fd2b37477d
Justin Beuck <jbeuck@yahoo.com>
2015-06-22 23:47:10 -0500

    added firebase to save users

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var bodyparser = require('body-parser');
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var Firebase = require("firebase");
 
+var ref = new Firebase("https://yulok.firebaseio.com/");
+
+app.use(bodyparser());
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/views/index.html'));
@@ -19,14 +24,27 @@ app.get('/signup',function(req,res){
   res.sendFile(path.join(__dirname+'/views/signup.html'));
 });
 
-app.post('/signupplayer', function(req, res) {
-  console.log(req);
+app.all('*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+   next();
 });
+
+app.post('/collectdata', function(req, res) {
+  var usersRef = ref.child("users");
+  usersRef.set({
+  fullname: req.body.fullName
+});
+  res.send(200);
+});
+
+
 
 app.use("/css", express.static(__dirname + '/css'));
 
 app.use("/js", express.static(__dirname + '/js'));
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log('listening...');
+  console.log('listening....');
 });
